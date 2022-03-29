44a97a842f3ec65df7be7c06c68731b98b8f54b4
hafidz <ahmad.hafidz98@yahoo.co.id>
2022-03-29 16:16:42 +0700

    apis

--- services/api.js
+++ services/api.js
@@ -1,8 +1,12 @@
 import axios from 'axios';
 import { BASE_URL } from '@env';
+import { AuthContext } from '../component/context';
 
 const api = axios.create({
-    baseURL: BASE_URL
+    baseURL: BASE_URL,
+    // headers: {
+        
+    // }
 })
 
 export default api;
